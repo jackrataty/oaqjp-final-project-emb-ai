fddab22b5eb2dee1e0f9319b2913d06cc211678a
jackrataty <jackrataty@gmail.com>
2026-01-29 00:52:55 -0500

    Initial Commit From Lab

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,12 +1,24 @@
 
-import requests
+from flask import Flask, request, jsonify
+import requests, json
 
 def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' # Mock API endpoint
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json = myobj, headers=headers)                             
+    response = requests.post(url, json = myobj, headers=headers)
+    data = response.json()                             
     if response.status_code == 200:
-        return response.text
+        emotions = data["emotionPredictions"][0]['emotion']
+        highest_emotion = max(emotions, key=emotions.get)
+        emotions['dominant_emotion'] = highest_emotion
+
+        print("=== MY OUTPUT ===")  # Add this
+        print(json.dumps(emotions, indent=2))
+
+        return emotions
     else:
         return {"error": "Failed to analyze emotion"}
+    
+    
