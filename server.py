61a5ae65d913c88bc73f150f4ab4a40103aff513
Jacqueline Jackson <jackrataty@gmail.com>
2026-01-30 19:07:02 -0500

    Update error handling

--- server.py
+++ server.py
@@ -13,12 +13,16 @@ app = Flask(__name__)
 def sent_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     emotion, dominant = emotion_detector(text_to_analyze)
-    display_statement = "For the given statement, the system response is "
-    
-    for key, value in emotion.items():
-        display_statement += f", {key} : {value}"
+    if !dominant:
+        display_statement = "Invalid text! Please try again!"
+        
+    else:
+        display_statement = "For the given statement, the system response is "
+        
+        for key, value in emotion.items():
+            display_statement += f", {key} : {value}"
 
-    display_statement = display_statement + f". The dominant emotion is {dominant}"
+        display_statement = display_statement + f". The dominant emotion is {dominant}"
 
     return  display_statement)
 
